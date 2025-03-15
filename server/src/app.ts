34675d9a2dbc98e81ceb71e274526d7ba73cc5a4
Fabricio Oliveira Lopes <Fabriciooliveiralopes50@gmail.com>
2025-03-15 11:42:28 -0300

    :sparkles: feat: Criei o modelo da Turma

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,15 @@
 import Express from "express";
 import database from "./config/database";
 import { configDotenv } from "dotenv";
+import routerAuth from "./routes/routerAuth";
 
 configDotenv();
 const app = Express();
+app.use(Express.json());
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
+
+app.use('/api', routerAuth)
 
 database.sync().then(() => {
   app.listen(PORT, () => {
---
+++ server/src/models/class.ts
@@ -0,0 +1,38 @@
+import { DataTypes, Model } from "sequelize";
+import database from "../config/database";
+
+class Class extends Model {
+  declare id: number;
+  declare name: string;
+  declare grade: number;
+  declare period: "Manhã" | "Tarde" | "Noite";
+}
+
+Class.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    grade: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    period: {
+      type: DataTypes.ENUM("Manhã", "Tarde", "Noite"),
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: database,
+    tableName: "class",
+    timestamps: false,
+  }
+);
+
+export default Class;
