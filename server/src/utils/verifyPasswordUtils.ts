b6fc361de9a80d32bf68eb9b6f58f29cb678b84c
Fabricio Oliveira Lopes <Fabriciooliveiralopes50@gmail.com>
2025-03-17 17:40:49 -0300

    :boom: fix: Arrumei o nome da função

--- server/src/utils/verifyPasswordUtils.ts
+++ server/src/utils/verifyPasswordUtils.ts
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 
-export async function hashedPassword(password: string, userPassword: string) {
+export async function verifyPassword(password: string, userPassword: string) {
   const isPasswordValid = await bcrypt.compare(password, userPassword);
   if (!isPasswordValid) {
     throw new Error("Senha incorreta");
