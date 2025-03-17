c42816835d8361046c916128f8bd398ae4532ab6
Fabricio Oliveira Lopes <Fabriciooliveiralopes50@gmail.com>
2025-03-17 20:05:18 -0300

    :sparkles: feat: Criei a rota de criar uma conta para um coordenador

--- server/src/routes/routerCreate.ts
+++ server/src/routes/routerCreate.ts
@@ -1,6 +1,7 @@
 import Express, { Request, Response } from "express";
 import studentController from "../controllers/studentController";
 import teacherController from "../controllers/teacherController";
+import coordinatorController from "../controllers/coordinatorController";
 
 const routerCreate = Express.Router();
 
@@ -11,6 +12,10 @@ routerCreate.post("/student", (req: Request, res: Response) => {
 
 routerCreate.post("/teacher", (req: Request, res: Response) => {
   teacherController.createTeacher(req, res);
+});
+
+routerCreate.post("/coordinator", (req: Request, res: Response) => {
+  coordinatorController.createCoordinator(req, res);
 });
 
 export default routerCreate;
