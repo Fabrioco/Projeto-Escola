73073b5b324cadf0e2ce831261a99473bd75aebb
Fabricio Oliveira Lopes <Fabriciooliveiralopes50@gmail.com>
2025-03-17 17:39:43 -0300

    :sparkles: feat: Rota para login do professor

--- server/src/routes/routerAuth.ts
+++ server/src/routes/routerAuth.ts
@@ -1,12 +1,16 @@
 import express, { Request, Response } from "express";
 import studentController from "../controllers/studentController";
+import teacherController from "../controllers/teacherController";
 
 const routerAuth = express.Router();
 
 // Rota de login
-routerAuth.post("/login", (req: Request, res: Response) => {
+routerAuth.post("/student/login", (req: Request, res: Response) => {
   studentController.login(req, res);
 });
 
+routerAuth.post("/teacher/login", (req: Request, res: Response) => {
+  teacherController.login(req, res);
+});
 
 export default routerAuth;
