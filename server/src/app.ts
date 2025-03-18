f9d50c1d7c3f5a1d130f21b9b2a378b837b06963
Fabricio Oliveira Lopes <Fabriciooliveiralopes50@gmail.com>
2025-03-18 12:17:57 -0300

    :sparkles: feat: Utilização das rotas da disciplina

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,6 +4,7 @@ import { configDotenv } from "dotenv";
 import routerAuth from "./routes/routerAuth";
 import routerCreate from "./routes/routerCreate";
 import routerClass from "./routes/routerClass";
+import routerDiscipline from "./routes/routerDiscipline";
 
 configDotenv();
 const app = Express();
@@ -14,6 +15,7 @@ const PORT = process.env.PORT || 5000;
 app.use("/api/auth", routerAuth);
 app.use("/api/create", routerCreate);
 app.use("/api/class", routerClass);
+app.use("/api/discipline", routerDiscipline);
 
 database.sync().then(() => {
   app.listen(PORT, () => {
