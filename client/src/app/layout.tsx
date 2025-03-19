a72deb46643c6d8ffdb0e2b34cc4662d83c58711
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-03-19 15:29:39 -0300

    feat: Criei o header

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+import { Header } from "@/ui/Header/page";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={`antialiased ${montserrat.className} bg-gray-200 w-screen min-h-screen flex flex-col items-center justify-center`}>
+      <body
+        className={`antialiased ${montserrat.className} bg-gray-200 w-screen min-h-screen flex flex-col items-center justify-center`}
+      >
+        <Header />
         {children}
       </body>
     </html>
---
+++ client/src/ui/Header/page.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header className="w-full h-16 bg-white text-blue-950 flex items-center justify-center text-2xl font-bold uppercase shadow-md shadow-blue-950/10">
+      <h1>E. E. João Paulo II</h1>
+    </header>
+  );
+}
