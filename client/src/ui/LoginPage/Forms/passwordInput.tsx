d161077f70ab86c7dff8295ac8eafbf566973c5b
Fabricio Oliveira Lopes <100289975+Fabrioco@users.noreply.github.com>
2025-03-20 00:08:04 +0000

    feat: Adicionei uma forma de ver a senha

--- client/src/ui/LoginPage/Forms/passwordInput.tsx
+++ client/src/ui/LoginPage/Forms/passwordInput.tsx
@@ -1,4 +1,7 @@
+"use client";
+import { EyeClosed } from "@phosphor-icons/react";
 import { Eye } from "@phosphor-icons/react/dist/ssr";
+import React from "react";
 
 type PasswordInputProps = {
   password: string;
@@ -6,6 +9,16 @@ type PasswordInputProps = {
 };
 
 export function PasswordInput({ password, setPassword }: PasswordInputProps) {
+  const [showPassword, setShowPassword] = React.useState<boolean>(false);
+
+  let typePassword: string = "password";
+
+  if (showPassword) {
+    typePassword = "text";
+  } else {
+    typePassword = "password";
+  }
+
   return (
     <div className="w-full flex relative group">
       <label
@@ -19,18 +32,28 @@ export function PasswordInput({ password, setPassword }: PasswordInputProps) {
         Senha
       </label>
       <input
-        type="password"
+        type={showPassword ? "text" : typePassword}
         name="password"
         id="password"
         className="w-full h-full focus:outline-none px-2 py-4 border border-blue-950 rounded-2xl focus:ring-blue-950 focus:border-blue-950 focus:ring-2 cursor-pointer"
         value={password}
         onChange={setPassword}
       />
-      <Eye
-        size={30}
-        className="cursor-pointer absolute top-3 right-2"
-        color="#162456"
-      />
+      {showPassword ? (
+        <Eye
+          size={30}
+          className="cursor-pointer absolute top-3 right-2"
+          color="#162456"
+          onClick={() => setShowPassword(!showPassword)}
+        />
+      ) : (
+        <EyeClosed
+          size={30}
+          className="cursor-pointer absolute top-3 right-2"
+          color="#162456"
+          onClick={() => setShowPassword(!showPassword)}
+        />
+      )}
     </div>
   );
 }
