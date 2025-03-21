23b7a7cc7eb366ad70a97263cbf3183a9c4fa4f1
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-03-21 12:32:02 -0300

    :boom: fix: Corrigi o useEffect para atualizar de acordo com o localStorage

--- client/src/contexts/authContext.tsx
+++ client/src/contexts/authContext.tsx
@@ -79,7 +79,7 @@ export const AuthProvider: React.FC<AuthContextProviderProps> = ({
 
   React.useEffect(() => {
     fetchUser();
-  }, []);
+  }, [localStorage.getItem("token")]);
 
   const fetchUser = async () => {
     const token = localStorage.getItem("token");
