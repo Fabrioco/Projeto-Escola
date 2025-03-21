60258ec67916b0e9307601fc163adb4d3e27c139
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-03-21 12:33:52 -0300

    :sparkles: feat: Criei a função de logout

--- client/src/components/logOutButton.tsx
+++ client/src/components/logOutButton.tsx
@@ -1,6 +1,20 @@
+"use client";
+
+import { useAuth } from "@/contexts/authContext";
+
 export function LogOutButton() {
+  const { logOut } = useAuth();
+
+  function handleLogout() {
+    logOut();
+  }
+
   return (
-    <button className="bg-red-400 hover:bg-red-600 text-white text-sm py-1 px-2 rounded-full">
+    <button
+      className="bg-red-400 hover:bg-red-600 text-white text-sm py-1 px-2 rounded-full"
+      type="button"
+      onClick={handleLogout}
+    >
       Sair
     </button>
   );
