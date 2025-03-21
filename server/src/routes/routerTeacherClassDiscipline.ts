caa56486e0e2adfe5fa0cbbfd77498ce6fdb271a
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-03-21 12:25:38 -0300

    :sparkles: feat: Criei as rotas de buscar por id, atualizar e deletar

--- server/src/routes/routerTeacherClassDiscipline.ts
+++ server/src/routes/routerTeacherClassDiscipline.ts
@@ -7,4 +7,20 @@ routerTeacherClassDiscipline.post("/", (req: Request, res: Response) => {
   teacherClassDisciplineController.createTeacherClassDiscipline(req, res);
 });
 
+routerTeacherClassDiscipline.get("/", (req: Request, res: Response) => {
+  teacherClassDisciplineController.getAllTeacherClassDisciplines(req, res);
+});
+
+routerTeacherClassDiscipline.get("/:id", (req: Request, res: Response) => {
+  teacherClassDisciplineController.getTeacherClassDisciplineById(req, res);
+});
+
+routerTeacherClassDiscipline.put("/:id", (req: Request, res: Response) => {
+  teacherClassDisciplineController.updateTeacherClassDiscipline(req, res);
+});
+
+routerTeacherClassDiscipline.delete("/:id", (req: Request, res: Response) => {
+  teacherClassDisciplineController.deleteTeacherClassDiscipline(req, res);
+});
+
 export default routerTeacherClassDiscipline;
