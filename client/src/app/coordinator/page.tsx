bcc080f4c75368b16e9bc81b05dc2f7b0be7bc7c
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-03-23 05:36:27 -0300

    :sparkles: feat: Criei o layout

--- client/src/app/coordinator/page.tsx
+++ client/src/app/coordinator/page.tsx
@@ -1,26 +1,23 @@
 "use client"; // Depois vou pôr no componente correto
 import { LogOutButton } from "@/components/logOutButton";
-import { useAuth } from "@/contexts/authContext";
 import ProtectedRoute from "@/routes/protectedRoute";
 import { AttributionDisciplineForm } from "@/ui/CoordinatorPage/AttribuitionDisciplineForm";
-import { ClassForm } from "@/ui/CoordinatorPage/ClassForm";
+import { ClassForm } from "@/ui/CoordinatorPage/ClassForm/ClassForm";
 import { CoordinatorForm } from "@/ui/CoordinatorPage/CoordinatorForm";
 import { DisciplineForm } from "@/ui/CoordinatorPage/DisciplineForm";
 import { StudentForm } from "@/ui/CoordinatorPage/StudentForm";
 import { TeacherForm } from "@/ui/CoordinatorPage/TeacherForm";
 
 export default function CoordinatorPage() {
-  const { user } = useAuth();
-  console.log(user);
   return (
-    <ProtectedRoute>
-      <ClassForm />
-      <StudentForm />
-      <TeacherForm />
-      <CoordinatorForm />
-      <DisciplineForm />
-      <AttributionDisciplineForm />
-      <LogOutButton />
-    </ProtectedRoute>
+      <ProtectedRoute>
+        <ClassForm />
+        <StudentForm />
+        <TeacherForm />
+        <CoordinatorForm />
+        <DisciplineForm />
+        <AttributionDisciplineForm />
+        <LogOutButton />
+      </ProtectedRoute>
   );
 }
