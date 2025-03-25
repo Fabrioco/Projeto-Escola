79375010e929f435c3389db7824213f9b70424a1
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-03-25 08:01:47 -0300

    :sparkles: feat: Adicionei toda a tipagem do context

--- client/src/types/StudentContextType.ts
+++ client/src/types/StudentContextType.ts
@@ -1,5 +1,33 @@
 import { ReactNode } from "react";
+import { UserProps } from "./AuthContextType";
+import { ClassesProps } from "./ClassContextType";
 
-export type StudentContextType = {};
+export type StudentContextType = {
+  nameStudent: string;
+  setNameStudent: React.Dispatch<React.SetStateAction<string>>;
+  email: string;
+  setEmail: React.Dispatch<React.SetStateAction<string>>;
+  password: string;
+  setPassword: React.Dispatch<React.SetStateAction<string>>;
+  classId: string;
+  setClassId: React.Dispatch<React.SetStateAction<string>>;
+  period: string;
+  setPeriod: React.Dispatch<React.SetStateAction<string>>;
+  students: UserProps[];
+  setStudents: React.Dispatch<React.SetStateAction<UserProps[]>>;
+  classesList: ClassesProps[];
+  setClassesList: React.Dispatch<React.SetStateAction<ClassesProps[]>>;
+  edit: boolean;
+  setEdit: React.Dispatch<React.SetStateAction<boolean>>;
+  userId: number;
+  setUserId: React.Dispatch<React.SetStateAction<number>>;
+  addStudent: () => void;
+  getAllStudents: () => void;
+  fetchClass: () => void;
+  deleteStudent: (id: number) => void;
+  handlePullData: (id: number) => void;
+  updateStudent: () => void;
+  cleanForm: () => void;
+};
 
 export type StudentProviderType = { children: ReactNode };
