292513f943c0423f88c9b2a69dc594bd509a0153
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-03-26 06:48:54 -0300

    :sparkles: feat: Criei a função de adicionar um coordenador e limpar o formulário

--- client/src/ui/CoordinatorPage/CoordinatorForm/CoordinatorForm.tsx
+++ client/src/ui/CoordinatorPage/CoordinatorForm/CoordinatorForm.tsx
@@ -1,16 +1,63 @@
+import axios from "axios";
+import React from "react";
+
 export function CoordinatorForm() {
+  const [name, setName] = React.useState<string>("");
+  const [email, setEmail] = React.useState<string>("");
+  const [password, setPassword] = React.useState<string>("");
+
+  const addCoordinator = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await axios
+      .post("http://localhost:5000/api/create/coordinator", {
+        name,
+        email,
+        password,
+      })
+      .then((res) => {
+        console.log(res.data);
+        clearForm();
+      })
+      .catch((error) => {
+        if (error instanceof axios.AxiosError && error.response) {
+          console.log("Erro na requisição", {
+            status: error.response.status,
+            data: error.response.data.error,
+            url: error.config?.url,
+          });
+        }
+      });
+  };
+
+  const clearForm = () => {
+    setName("");
+    setEmail("");
+    setPassword("");
+  };
   return (
     <div>
       <h1>Coordenadores</h1>
-      <form action="">
+      <form action="" onSubmit={addCoordinator}>
         <div>
           <label htmlFor="nameCoordinator">Nome</label>
-          <input type="text" id="nameCoordinator" name="nameCoordinator" />
+          <input
+            type="text"
+            id="nameCoordinator"
+            name="nameCoordinator"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
 
         <div>
           <label htmlFor="emailCoordinator">Email</label>
-          <input type="email" id="emailCoordinator" name="emailCoordinator" />
+          <input
+            type="email"
+            id="emailCoordinator"
+            name="emailCoordinator"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
 
         <div>
@@ -19,11 +66,16 @@ export function CoordinatorForm() {
             type="password"
             id="passwordCoordinator"
             name="passwordCoordinator"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
 
         <button type="submit">Cadastrar</button>
       </form>
+      <button type="button" onClick={clearForm}>
+        limpar
+      </button>
     </div>
   );
 }
