8b585902297010787274e85ade5eab20776ceb18
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-03-26 06:42:04 -0300

    :book: docs: Separei em pasta

--- client/src/app/coordinator/page.tsx
+++ client/src/app/coordinator/page.tsx
@@ -6,7 +6,7 @@ import { TeacherProvider } from "@/contexts/teacherContext";
 import ProtectedRoute from "@/routes/protectedRoute";
 import { AttributionDisciplineForm } from "@/ui/CoordinatorPage/AttribuitionDisciplineForm";
 import { ClassForm } from "@/ui/CoordinatorPage/ClassForm/ClassForm";
-import { CoordinatorForm } from "@/ui/CoordinatorPage/CoordinatorForm";
+import { CoordinatorForm } from "@/ui/CoordinatorPage/CoordinatorForm/CoordinatorForm";
 import { DisciplineForm } from "@/ui/CoordinatorPage/DisciplineForm";
 import { StudentForm } from "@/ui/CoordinatorPage/StudentForm/StudentForm";
 import { TeacherForm } from "@/ui/CoordinatorPage/TeacherForm/teacherForm";
