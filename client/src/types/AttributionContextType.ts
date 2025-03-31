7bfa9d0d861220a422a5d61965c3d96cc7e1c1db
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-03-31 19:14:32 -0300

    :sparkles: feat: Adicionei mais funções

--- client/src/types/AttributionContextType.ts
+++ client/src/types/AttributionContextType.ts
@@ -1,7 +1,29 @@
 export type AttributionContextType = {
   fetchAllAttributions: () => Promise<void>;
+  allAttributions: AttributionPropsType[];
+  addAttribution: (e: React.FormEvent) => Promise<void>;
+  fetchAttribution: (id: number) => Promise<void>;
+  updateAttribution: (e: React.FormEvent) => Promise<void>;
+  deleteAttribution: (id: number) => Promise<void>;
+  teacherId: string;
+  setTeacherId: React.Dispatch<React.SetStateAction<string>>;
+  classId: string;
+  setClassId: React.Dispatch<React.SetStateAction<string>>;
+  disciplineId: string;
+  setDisciplineId: React.Dispatch<React.SetStateAction<string>>;
+  timeId: string;
+  setTimeId: React.Dispatch<React.SetStateAction<string>>;
+  edit: boolean;
 };
 
 export type AttributionProviderType = {
   children: React.ReactNode;
+};
+
+export type AttributionPropsType = {
+  id: number;
+  teacher_id: number;
+  class_id: number;
+  discipline_id: number;
+  time_id: number;
 };
