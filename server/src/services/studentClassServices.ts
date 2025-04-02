c1529b73b18baafcabe222c7ee0c134d722e86bd
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-04-02 12:43:17 -0300

    :sparkles: feat: Criei o service de remover um aluno da sala

--- server/src/services/studentClassServices.ts
+++ server/src/services/studentClassServices.ts
@@ -12,5 +12,19 @@ class StudentClassServices {
       return error;
     }
   }
+
+  static async delete(student_id: number, class_id: number) {
+    try {
+      const studentClass = await StudentClass.destroy({
+        where: {
+          student_id,
+          class_id,
+        },
+      });
+      return studentClass;
+    } catch (error) {
+      return error;
+    }
+  }
 }
 export default StudentClassServices;
