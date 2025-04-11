04ac2f06c04a6bdef69476f5f9229360e3f5e758
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-04-11 19:31:34 -0300

    feat: Adicionei o service de criar a conta do estudante

--- server/src/students/students.service.ts
+++ server/src/students/students.service.ts
@@ -1,13 +1,35 @@
-import { Injectable } from '@nestjs/common';
-import { CreateStudentDto } from './dto/create-student.dto';
-import { UpdateStudentDto } from './dto/update-student.dto';
+import { ConflictException, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import * as bcrypt from "bcrypt";
+import { Student } from "./entities/student.entity";
+import { CreateStudentDto } from "./dto/create-student.dto";
+import { UpdateStudentDto } from "./dto/update-student.dto";
 
 @Injectable()
 export class StudentsService {
-  create(createStudentDto: CreateStudentDto) {
-    return 'This action adds a new student';
-  }
+  constructor(
+    @InjectRepository(Student)
+    private UserRepository: Repository<Student>,
+  ) {}
 
+  async create(createStudentDto: CreateStudentDto): Promise<string> {
+    try {
+      const verifyEmail = await this.UserRepository.findOne({ where: { email: createStudentDto.email } });
+      if (verifyEmail) {
+        throw new ConflictException("Email já cadastrado");
+      }
+
+      const hashPassword = await bcrypt.hash(createStudentDto.password, 10);
+
+      const student = await this.UserRepository.create({ ...createStudentDto, password: hashPassword });
+      await this.UserRepository.save(student);
+
+      return "Cadastro realizado com sucesso";
+    } catch (error) {
+      throw new ConflictException(error);
+    }
+  }
   findAll() {
     return `This action returns all students`;
   }
