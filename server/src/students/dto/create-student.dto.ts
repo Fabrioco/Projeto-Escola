b0f7fae144c1d896535d629ec556a4799f92d3e6
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-04-11 19:03:13 -0300

    feat: Criei o modelo do student

---
+++ server/src/students/dto/create-student.dto.ts
@@ -0,0 +1,6 @@
+export class CreateStudentDto {
+  name: string;
+  email: string;
+  password: string;
+  class_id: number;
+}
---
+++ server/src/students/entities/student.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Student {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  class_id: number;
+}
