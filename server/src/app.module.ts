8ebb8bb058eb982cebde1c0698d82117cf336a70
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-04-11 19:01:57 -0300

    feat: Adicionei outras configurações do .env do banco de dados

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,6 +2,9 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
+import { StudentsModule } from "./students/students.module";
+import { ClassesModule } from "./classes/classes.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
   imports: [
@@ -9,6 +12,19 @@ import { ConfigModule } from "@nestjs/config";
       isGlobal: true,
       envFilePath: ".env",
     }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT !== undefined ? parseInt(process.env.DB_PORT) : 5000,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+      entities: ["dist/**/*.entity\.ts"],
+    }),
+    StudentsModule,
+    ClassesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
