84ac68128af4b599f713f0c803e88d815ca21afc
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-04-11 19:38:15 -0300

    feat: Adicionei a entidade para o banco de dados

--- server/src/coordinators/coordinators.module.ts
+++ server/src/coordinators/coordinators.module.ts
@@ -1,8 +1,11 @@
-import { Module } from '@nestjs/common';
-import { CoordinatorsService } from './coordinators.service';
-import { CoordinatorsController } from './coordinators.controller';
+import { Module } from "@nestjs/common";
+import { CoordinatorsService } from "./coordinators.service";
+import { CoordinatorsController } from "./coordinators.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Coordinator } from "./entities/coordinator.entity";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Coordinator])],
   controllers: [CoordinatorsController],
   providers: [CoordinatorsService],
 })
