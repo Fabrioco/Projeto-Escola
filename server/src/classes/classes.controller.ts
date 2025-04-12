b3dcb726672edc721919ac184a2f4a3bff4c8e53
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-04-12 14:21:08 -0300

    feat: Criei a função de adicionar uma nova turma

--- server/src/classes/classes.controller.ts
+++ server/src/classes/classes.controller.ts
@@ -1,14 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { ClassesService } from './classes.service';
-import { CreateClassDto } from './dto/create-class.dto';
-import { UpdateClassDto } from './dto/update-class.dto';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ConflictException, UseGuards } from "@nestjs/common";
+import { ClassesService } from "./classes.service";
+import { CreateClassDto } from "./dto/create-class.dto";
+import { UpdateClassDto } from "./dto/update-class.dto";
+import { AuthGuard } from "src/auth/auth.guard";
 
-@Controller('classes')
+@Controller("classes")
+@UseGuards(AuthGuard)
 export class ClassesController {
   constructor(private readonly classesService: ClassesService) {}
 
   @Post()
   create(@Body() createClassDto: CreateClassDto) {
+    if (!createClassDto.name || !createClassDto.grade || !createClassDto.period) {
+      throw new ConflictException("Preencha todos os campos");
+    }
     return this.classesService.create(createClassDto);
   }
 
@@ -17,18 +22,18 @@ export class ClassesController {
     return this.classesService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get(":id")
+  findOne(@Param("id") id: string) {
     return this.classesService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateClassDto: UpdateClassDto) {
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updateClassDto: UpdateClassDto) {
     return this.classesService.update(+id, updateClassDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @Delete(":id")
+  remove(@Param("id") id: string) {
     return this.classesService.remove(+id);
   }
 }
--- server/src/classes/classes.service.ts
+++ server/src/classes/classes.service.ts
@@ -1,11 +1,30 @@
-import { Injectable } from '@nestjs/common';
-import { CreateClassDto } from './dto/create-class.dto';
-import { UpdateClassDto } from './dto/update-class.dto';
+import { ConflictException, Injectable } from "@nestjs/common";
+import { CreateClassDto } from "./dto/create-class.dto";
+import { UpdateClassDto } from "./dto/update-class.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Class } from "./entities/class.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class ClassesService {
-  create(createClassDto: CreateClassDto) {
-    return 'This action adds a new class';
+  constructor(
+    @InjectRepository(Class)
+    private classRepository: Repository<Class>,
+  ) {}
+  async create(createClassDto: CreateClassDto) {
+    try {
+      const verifyClass = await this.classRepository.findOne({ where: { name: createClassDto.name, grade: createClassDto.grade, period: createClassDto.period } });
+      if (verifyClass) {
+        throw new ConflictException("Turma já cadastrada");
+      }
+
+      const newClass = await this.classRepository.create(createClassDto);
+      await this.classRepository.save(newClass);
+
+      return "Turma cadastrada com sucesso";
+    } catch (error) {
+      throw new ConflictException(error);
+    }
   }
 
   findAll() {
