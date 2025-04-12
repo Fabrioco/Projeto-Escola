4bf28b3f8194db11f187d3808a32628941367f6e
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-04-11 22:56:44 -0300

    feat: função de login do usuário de acordo com a role

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,11 +1,45 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import * as jwt from "jsonwebtoken";
+import { ConflictException, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Coordinator } from "src/coordinators/entities/coordinator.entity";
+import { Student } from "src/students/entities/student.entity";
+import { Repository } from "typeorm";
+import { SigninAuthDto } from "./dto/signin-auth";
+import { JwtService } from "@nestjs/jwt";
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    @InjectRepository(Coordinator)
+    private authRepository: Repository<Coordinator>,
+
+    @InjectRepository(Student)
+    private studentRepository: Repository<Student>,
+
+    private jwtService: JwtService,
+  ) {}
+
+  async signIn(createAuthDto: SigninAuthDto) {
+    if (createAuthDto.role === "student") {
+      const student = await this.studentRepository.findOne({
+        where: { email: createAuthDto.email },
+      });
+      if (!student) {
+        throw new ConflictException("Estudante não encontrado");
+      }
+      const token = await this.jwtService.signAsync({ id: student.id, role: "student" });
+      return { token };
+    } else if (createAuthDto.role === "coordinator") {
+      const coordinator = await this.authRepository.findOne({
+        where: { email: createAuthDto.email },
+      });
+      if (!coordinator) {
+        throw new ConflictException("Coordenador não encontrado");
+      }
+      const token = await this.jwtService.signAsync({ id: coordinator.id, role: "coordinator" });
+      console.log(this.jwtService.decode(token));
+      return { token };
+    }
   }
 
   findAll() {
@@ -14,10 +48,6 @@ export class AuthService {
 
   findOne(id: number) {
     return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
   }
 
   remove(id: number) {
