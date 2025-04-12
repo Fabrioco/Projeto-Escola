23f74855ad9bc61d0c4b7242c998e411eabaff17
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-04-12 14:20:35 -0300

    feat: Adicionei a configuração do jwtModule

--- server/src/classes/classes.module.ts
+++ server/src/classes/classes.module.ts
@@ -3,9 +3,16 @@ import { ClassesService } from "./classes.service";
 import { ClassesController } from "./classes.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Class } from "./entities/class.entity";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Class])],
+  imports: [
+    TypeOrmModule.forFeature([Class]),
+    JwtModule.register({
+      secret: "secret",
+      signOptions: { expiresIn: "1d" },
+    }),
+  ],
   controllers: [ClassesController],
   providers: [ClassesService],
 })
