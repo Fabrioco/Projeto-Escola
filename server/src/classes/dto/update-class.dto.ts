d693391eaf392232ca2c511551027877fa622238
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-04-12 13:38:00 -0300

    feat: Adicionei a tipagem das classes

--- server/src/classes/dto/update-class.dto.ts
+++ server/src/classes/dto/update-class.dto.ts
@@ -1,4 +1,8 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateClassDto } from './create-class.dto';
+import { PartialType } from "@nestjs/mapped-types";
+import { CreateClassDto } from "./create-class.dto";
 
-export class UpdateClassDto extends PartialType(CreateClassDto) {}
+export class UpdateClassDto extends PartialType(CreateClassDto) {
+  name?: string | undefined;
+  grade?: number | undefined;
+  period?: number | undefined;
+}
