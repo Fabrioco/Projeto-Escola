2bef529572834a460770f36815d61cee0382632e
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-04-15 19:34:09 -0300

    feat: Adicionei o JWTModule

--- server/src/schedules/schedules.module.ts
+++ server/src/schedules/schedules.module.ts
@@ -3,9 +3,16 @@ import { SchedulesService } from "./schedules.service";
 import { SchedulesController } from "./schedules.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Schedule } from "./entities/schedule.entity";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Schedule])],
+  imports: [
+    TypeOrmModule.forFeature([Schedule]),
+    JwtModule.register({
+      secret: "secret",
+      signOptions: { expiresIn: "1d" },
+    }),
+  ],
   controllers: [SchedulesController],
   providers: [SchedulesService],
 })
