f8b7a2a7bed90e5bfbbcc6f0218ac284afaf0a3c
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-04-15 19:20:16 -0300

    feat: Criei o entity

--- server/src/schedules/entities/schedule.entity.ts
+++ server/src/schedules/entities/schedule.entity.ts
@@ -1,1 +1,13 @@
-export class Schedule {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Schedule {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  startTime: string;
+
+  @Column()
+  endTime: string;
+}
