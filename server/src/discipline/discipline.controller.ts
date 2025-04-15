1a369fc0044938cf26044928612550c2460cdfd3
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-04-15 20:43:43 -0300

    feat: Adicionei o AuthGuard e RolesGuard

--- server/src/discipline/discipline.controller.ts
+++ server/src/discipline/discipline.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { DisciplineService } from './discipline.service';
-import { CreateDisciplineDto } from './dto/create-discipline.dto';
-import { UpdateDisciplineDto } from './dto/update-discipline.dto';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from "@nestjs/common";
+import { DisciplineService } from "./discipline.service";
+import { CreateDisciplineDto } from "./dto/create-discipline.dto";
+import { UpdateDisciplineDto } from "./dto/update-discipline.dto";
+import { AuthGuard } from "src/auth/auth.guard";
+import { RolesGuard } from "src/auth/roles.guard";
+import { Roles } from "src/auth/roles.decorator";
 
-@Controller('discipline')
+@Controller("discipline")
+@UseGuards(AuthGuard, RolesGuard)
+@Roles("coordinator")
 export class DisciplineController {
   constructor(private readonly disciplineService: DisciplineService) {}
 
@@ -17,18 +22,18 @@ export class DisciplineController {
     return this.disciplineService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get(":id")
+  findOne(@Param("id") id: string) {
     return this.disciplineService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateDisciplineDto: UpdateDisciplineDto) {
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updateDisciplineDto: UpdateDisciplineDto) {
     return this.disciplineService.update(+id, updateDisciplineDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @Delete(":id")
+  remove(@Param("id") id: string) {
     return this.disciplineService.remove(+id);
   }
 }
--- server/src/discipline/discipline.module.ts
+++ server/src/discipline/discipline.module.ts
@@ -3,9 +3,16 @@ import { DisciplineService } from "./discipline.service";
 import { DisciplineController } from "./discipline.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Discipline } from "./entities/discipline.entity";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Discipline])],
+  imports: [
+    TypeOrmModule.forFeature([Discipline]),
+    JwtModule.register({
+      secret: "secret",
+      signOptions: { expiresIn: "1d" },
+    }),
+  ],
   controllers: [DisciplineController],
   providers: [DisciplineService],
 })
