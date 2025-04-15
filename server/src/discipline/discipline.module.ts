f99dc26a81d527f59be1bf4c8f0bf9b90e34e261
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-04-15 20:35:57 -0300

    feat: Adicionei o entity Discipline

--- server/src/discipline/discipline.module.ts
+++ server/src/discipline/discipline.module.ts
@@ -1,8 +1,11 @@
-import { Module } from '@nestjs/common';
-import { DisciplineService } from './discipline.service';
-import { DisciplineController } from './discipline.controller';
+import { Module } from "@nestjs/common";
+import { DisciplineService } from "./discipline.service";
+import { DisciplineController } from "./discipline.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Discipline } from "./entities/discipline.entity";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Discipline])],
   controllers: [DisciplineController],
   providers: [DisciplineService],
 })
