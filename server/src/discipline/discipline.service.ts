0663588fd93593ac3d4679f48f0c57a915a9b1e6
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-04-15 20:36:32 -0300

    feat: Criei o service de criar disciplina

--- server/src/discipline/discipline.service.ts
+++ server/src/discipline/discipline.service.ts
@@ -1,11 +1,33 @@
-import { Injectable } from '@nestjs/common';
-import { CreateDisciplineDto } from './dto/create-discipline.dto';
-import { UpdateDisciplineDto } from './dto/update-discipline.dto';
+import { ConflictException, Injectable, InternalServerErrorException } from "@nestjs/common";
+import { CreateDisciplineDto } from "./dto/create-discipline.dto";
+import { UpdateDisciplineDto } from "./dto/update-discipline.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Discipline } from "./entities/discipline.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class DisciplineService {
-  create(createDisciplineDto: CreateDisciplineDto) {
-    return 'This action adds a new discipline';
+  constructor(
+    @InjectRepository(Discipline)
+    private disciplineRepository: Repository<Discipline>,
+  ) {}
+
+  async create(createDisciplineDto: CreateDisciplineDto): Promise<string> {
+    try {
+      const findDiscipline = await this.disciplineRepository.findOneBy({
+        name: createDisciplineDto.name,
+      });
+      if (findDiscipline) throw new ConflictException("Disciplina já cadastrada");
+
+      const discipline = this.disciplineRepository.create(createDisciplineDto);
+      await this.disciplineRepository.save(discipline);
+      return "Disciplina cadastrada com sucesso";
+    } catch (error) {
+      if (error instanceof ConflictException) {
+        throw error;
+      }
+      throw new InternalServerErrorException(error);
+    }
   }
 
   findAll() {
