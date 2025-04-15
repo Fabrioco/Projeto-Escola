c68e7cc207bf356612fbbd8efcccf7a0e1be5f97
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-04-15 19:35:48 -0300

    feat: Adicionei o useGuards e o Roles

--- server/src/schedules/schedules.controller.ts
+++ server/src/schedules/schedules.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { SchedulesService } from './schedules.service';
-import { CreateScheduleDto } from './dto/create-schedule.dto';
-import { UpdateScheduleDto } from './dto/update-schedule.dto';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from "@nestjs/common";
+import { SchedulesService } from "./schedules.service";
+import { CreateScheduleDto } from "./dto/create-schedule.dto";
+import { UpdateScheduleDto } from "./dto/update-schedule.dto";
+import { AuthGuard } from "src/auth/auth.guard";
+import { RolesGuard } from "src/auth/roles.guard";
+import { Roles } from "src/auth/roles.decorator";
 
-@Controller('schedules')
+@Controller("schedules")
+@UseGuards(AuthGuard, RolesGuard)
+@Roles("admin")
 export class SchedulesController {
   constructor(private readonly schedulesService: SchedulesService) {}
 
@@ -17,18 +22,18 @@ export class SchedulesController {
     return this.schedulesService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get(":id")
+  findOne(@Param("id") id: string) {
     return this.schedulesService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateScheduleDto: UpdateScheduleDto) {
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updateScheduleDto: UpdateScheduleDto) {
     return this.schedulesService.update(+id, updateScheduleDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @Delete(":id")
+  remove(@Param("id") id: string) {
     return this.schedulesService.remove(+id);
   }
 }
