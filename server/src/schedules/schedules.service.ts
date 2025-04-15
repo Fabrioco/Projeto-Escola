30d04164e2244b7cb8d9b2f92133f014a0b6f6b0
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-04-15 19:28:29 -0300

    feat: Criei o service de criar horário

--- server/src/schedules/schedules.service.ts
+++ server/src/schedules/schedules.service.ts
@@ -1,11 +1,30 @@
-import { Injectable } from '@nestjs/common';
-import { CreateScheduleDto } from './dto/create-schedule.dto';
-import { UpdateScheduleDto } from './dto/update-schedule.dto';
+import { ConflictException, Injectable, InternalServerErrorException, NotFoundException } from "@nestjs/common";
+import { CreateScheduleDto } from "./dto/create-schedule.dto";
+import { UpdateScheduleDto } from "./dto/update-schedule.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Schedule } from "./entities/schedule.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class SchedulesService {
-  create(createScheduleDto: CreateScheduleDto) {
-    return 'This action adds a new schedule';
+  constructor(@InjectRepository(Schedule) private scheduleRepository: Repository<Schedule>) {}
+
+  async create(createScheduleDto: CreateScheduleDto): Promise<string> {
+    try {
+      const findSchedule = await this.scheduleRepository.findOne({ where: { startTime: createScheduleDto.startTime, endTime: createScheduleDto.endTime } });
+      if (findSchedule) {
+        throw new ConflictException("Horário já cadastrado");
+      }
+
+      const newSchedule = await this.scheduleRepository.create(createScheduleDto);
+      await this.scheduleRepository.save(newSchedule);
+      return "Horário cadastrado com sucesso";
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+      throw new InternalServerErrorException("Falha ao cadastrar o horário", { cause: error, description: "Verifique os dados e tente novamente" });
+    }
   }
 
   findAll() {
