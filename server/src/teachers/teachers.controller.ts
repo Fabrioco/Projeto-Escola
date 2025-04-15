528e73588da210d67944ecc17135cf8bb4815440
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-04-15 18:16:08 -0300

    feat: Adicionei o AuthGuard para verificar se esta autenticado e o RolesGuard para verificação de cargo

--- server/src/teachers/teachers.controller.ts
+++ server/src/teachers/teachers.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { TeachersService } from './teachers.service';
-import { CreateTeacherDto } from './dto/create-teacher.dto';
-import { UpdateTeacherDto } from './dto/update-teacher.dto';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from "@nestjs/common";
+import { TeachersService } from "./teachers.service";
+import { CreateTeacherDto } from "./dto/create-teacher.dto";
+import { UpdateTeacherDto } from "./dto/update-teacher.dto";
+import { Roles } from "src/auth/roles.decorator";
+import { AuthGuard } from "src/auth/auth.guard";
+import { RolesGuard } from "src/auth/roles.guard";
 
-@Controller('teachers')
+@Controller("teachers")
+@UseGuards(AuthGuard, RolesGuard)
+@Roles("coordinator")
 export class TeachersController {
   constructor(private readonly teachersService: TeachersService) {}
 
@@ -17,18 +22,18 @@ export class TeachersController {
     return this.teachersService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get(":id")
+  findOne(@Param("id") id: string) {
     return this.teachersService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTeacherDto: UpdateTeacherDto) {
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updateTeacherDto: UpdateTeacherDto) {
     return this.teachersService.update(+id, updateTeacherDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @Delete(":id")
+  remove(@Param("id") id: string) {
     return this.teachersService.remove(+id);
   }
 }
