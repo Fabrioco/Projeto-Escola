838687b5a48c5397f8cfbc10bb7653605fa0d5bc
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-04-15 20:26:53 -0300

    fix: Adicionei o entity do Attribution

--- server/src/schedules/schedules.module.ts
+++ server/src/schedules/schedules.module.ts
@@ -4,10 +4,11 @@ import { SchedulesController } from "./schedules.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Schedule } from "./entities/schedule.entity";
 import { JwtModule } from "@nestjs/jwt";
+import { Attribution } from "src/attribution/entities/attribution.entity";
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Schedule]),
+    TypeOrmModule.forFeature([Schedule, Attribution]),
     JwtModule.register({
       secret: "secret",
       signOptions: { expiresIn: "1d" },
--- server/src/teachers/teachers.module.ts
+++ server/src/teachers/teachers.module.ts
@@ -4,10 +4,11 @@ import { TeachersController } from "./teachers.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Teacher } from "./entities/teacher.entity";
 import { JwtModule } from "@nestjs/jwt";
+import { Attribution } from "src/attribution/entities/attribution.entity";
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Teacher]),
+    TypeOrmModule.forFeature([Teacher, Attribution]),
     JwtModule.register({
       secret: "secret",
       signOptions: { expiresIn: "1d" },
