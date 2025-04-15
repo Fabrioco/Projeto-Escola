253e3534dc18a9a2ce21b05ee6b657211415ca40
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-04-15 15:52:20 -0300

    feat: Criei o enrity do banco de dados

--- server/src/teachers/entities/teacher.entity.ts
+++ server/src/teachers/entities/teacher.entity.ts
@@ -1,1 +1,19 @@
-export class Teacher {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Teacher {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ default: "teacher" })
+  role: string;
+}
