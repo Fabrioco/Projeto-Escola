2f7dddaf235ed7d77f3fc4030e4dc9d1bf9d5afc
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-04-15 23:49:12 -0300

    feat: Adicionei a tipagem

--- server/src/notes/dto/update-note.dto.ts
+++ server/src/notes/dto/update-note.dto.ts
@@ -1,4 +1,11 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateNoteDto } from './create-note.dto';
+import { PartialType } from "@nestjs/mapped-types";
+import { CreateNoteDto } from "./create-note.dto";
 
-export class UpdateNoteDto extends PartialType(CreateNoteDto) {}
+export class UpdateNoteDto extends PartialType(CreateNoteDto) {
+  discipline_id?: number | undefined;
+  teacher_id?: number | undefined;
+  student_id?: number | undefined;
+  note?: number | undefined;
+  date?: Date | undefined;
+  approved?: boolean | undefined;
+}
