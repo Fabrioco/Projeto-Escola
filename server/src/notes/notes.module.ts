517ea28f94f97d3b270f039fcc89a1d5ba64d58b
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-04-16 01:01:30 -0300

    feat: Adicionei os entities do estudante, professor e disciplina

--- server/src/notes/notes.module.ts
+++ server/src/notes/notes.module.ts
@@ -3,9 +3,12 @@ import { NotesService } from "./notes.service";
 import { NotesController } from "./notes.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Note } from "./entities/note.entity";
+import { Student } from "src/students/entities/student.entity";
+import { Teacher } from "src/teachers/entities/teacher.entity";
+import { Discipline } from "src/discipline/entities/discipline.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Note])],
+  imports: [TypeOrmModule.forFeature([Note, Discipline, Teacher, Student])],
   controllers: [NotesController],
   providers: [NotesService],
 })
