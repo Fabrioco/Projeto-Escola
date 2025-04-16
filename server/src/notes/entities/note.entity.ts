109bba738d28f0af7b0b0f5e444fcc746d5c0b4f
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-04-16 01:03:25 -0300

    feat: Adicionei as colunas

--- server/src/notes/entities/note.entity.ts
+++ server/src/notes/entities/note.entity.ts
@@ -12,13 +12,22 @@ export class Note {
   @JoinColumn({ name: "discipline_id" })
   disciplines: Discipline;
 
+  @Column()
+  discipline_id: number;
+
   @ManyToOne(() => Teacher, teacher => teacher.notes)
   @JoinColumn({ name: "teacher_id" })
   teachers: Teacher;
 
+  @Column()
+  teacher_id: number;
+
   @ManyToOne(() => Student, student => student.notes)
   @JoinColumn({ name: "student_id" })
   students: Student;
+
+  @Column()
+  student_id: number;
 
   @Column()
   note: number;
