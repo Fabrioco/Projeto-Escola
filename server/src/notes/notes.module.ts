98df346cb0e960a341d5aa82000409bc9d13f7b3
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-04-16 01:06:42 -0300

    feat: Adicionei o AuthGuard e o JWTModule

--- server/src/notes/notes.module.ts
+++ server/src/notes/notes.module.ts
@@ -6,9 +6,17 @@ import { Note } from "./entities/note.entity";
 import { Student } from "src/students/entities/student.entity";
 import { Teacher } from "src/teachers/entities/teacher.entity";
 import { Discipline } from "src/discipline/entities/discipline.entity";
+import { JwtModule } from "@nestjs/jwt";
+import { AuthGuard } from "src/auth/auth.guard";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Note, Discipline, Teacher, Student])],
+  imports: [
+    TypeOrmModule.forFeature([Note, Discipline, Teacher, Student, AuthGuard]),
+    JwtModule.register({
+      secret: "secret",
+      signOptions: { expiresIn: "1d" },
+    }),
+  ],
   controllers: [NotesController],
   providers: [NotesService],
 })
