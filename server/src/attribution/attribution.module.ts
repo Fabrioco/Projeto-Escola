df0fc1198074d14a0a5eb68556595cca04866eac
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-04-17 03:25:10 -0300

    feat: Adicionei os modules Attribution, Class, Teacher, Discipline e Schedule

--- server/src/attribution/attribution.module.ts
+++ server/src/attribution/attribution.module.ts
@@ -1,8 +1,15 @@
-import { Module } from '@nestjs/common';
-import { AttributionService } from './attribution.service';
-import { AttributionController } from './attribution.controller';
+import { Module } from "@nestjs/common";
+import { AttributionService } from "./attribution.service";
+import { AttributionController } from "./attribution.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Attribution } from "./entities/attribution.entity";
+import { Class } from "src/classes/entities/class.entity";
+import { Teacher } from "src/teachers/entities/teacher.entity";
+import { Discipline } from "src/discipline/entities/discipline.entity";
+import { Schedule } from "src/schedules/entities/schedule.entity";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Attribution, Class, Teacher, Discipline, Schedule])],
   controllers: [AttributionController],
   providers: [AttributionService],
 })
