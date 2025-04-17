18227c976571328dab0f8ba6175d81fc96697da7
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-04-17 01:25:28 -0300

    feat: Adicionei os entities da disciplina, estudante e professor

--- server/src/presences/presences.module.ts
+++ server/src/presences/presences.module.ts
@@ -1,8 +1,14 @@
-import { Module } from '@nestjs/common';
-import { PresencesService } from './presences.service';
-import { PresencesController } from './presences.controller';
+import { Module } from "@nestjs/common";
+import { PresencesService } from "./presences.service";
+import { PresencesController } from "./presences.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Presence } from "./entities/presence.entity";
+import { Discipline } from "src/discipline/entities/discipline.entity";
+import { Student } from "src/students/entities/student.entity";
+import { Teacher } from "src/teachers/entities/teacher.entity";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Presence, Discipline, Student, Teacher])],
   controllers: [PresencesController],
   providers: [PresencesService],
 })
