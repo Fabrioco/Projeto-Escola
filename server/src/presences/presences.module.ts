d456d90092c6d75805a53f26371262b42ac47929
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-04-17 02:18:54 -0300

    feat: Adicionei as configurações do JwtModule

--- server/src/presences/presences.module.ts
+++ server/src/presences/presences.module.ts
@@ -6,9 +6,16 @@ import { Presence } from "./entities/presence.entity";
 import { Discipline } from "src/discipline/entities/discipline.entity";
 import { Student } from "src/students/entities/student.entity";
 import { Teacher } from "src/teachers/entities/teacher.entity";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Presence, Discipline, Student, Teacher])],
+  imports: [
+    TypeOrmModule.forFeature([Presence, Discipline, Student, Teacher]),
+    JwtModule.register({
+      secret: "secret",
+      signOptions: { expiresIn: "1d" },
+    }),
+  ],
   controllers: [PresencesController],
   providers: [PresencesService],
 })
